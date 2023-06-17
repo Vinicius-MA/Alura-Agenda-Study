e2be434a7e4f4dee3d45941f0d09e2b4c6bed834
Vinícius M. Andreghetti <vinicius.andreghetti@gmail.com>
2023-06-17 10:55:07 -0300

    prepare database to revert migration v1 to v2 (v2 to v3), i.e. remove surname

--- app/src/main/java/br/com/vinma/agenda/room/AgendaDataBase.java
+++ app/src/main/java/br/com/vinma/agenda/room/AgendaDataBase.java
@@ -29,7 +29,21 @@ public abstract class AgendaDataBase extends RoomDatabase {
                         public void migrate(@NonNull SupportSQLiteDatabase database) {
                             database.execSQL("ALTER TABLE student ADD COLUMN surname TEXT");
                         }
-                    })
+                    },
+                            // Revert the migration from db v1 to v2 (remove surname)
+                            new Migration(2, 3) {
+                                @Override
+                                public void migrate(@NonNull SupportSQLiteDatabase database) {
+                                    // 1. Create a new table with desired information (id, name, phone, email)
+                                    database.execSQL("CREATE TABLE IF NOT EXISTS `Student_New` (`id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, `name` TEXT, `phone` TEXT, `email` TEXT)");
+                                    // 2. Copy all data from old table to new table
+                                    database.execSQL("INSERT INTO Student_New (id, name, phone, email) SELECT id, name, phone, email FROM Student");
+                                    // 3. Delete the old table
+                                    database.execSQL("DROP TABLE Student");
+                                    // 4. Rename the new table
+                                    database.execSQL("ALTER TABLE Student_New RENAME TO Student");
+                                }
+                            })
                     .build();
         }
         return instance;
